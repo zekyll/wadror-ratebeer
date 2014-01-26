26bad0da58806f7f3b1e75d3458686e1f974a780
Timo <timjlah@gmail.com>
2014-01-26 23:01:18 +0200

    Week 2 exercise 14.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def average_rating
+    return ratings.average(:score).to_f;
+  end
 end
