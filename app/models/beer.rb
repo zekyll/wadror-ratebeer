529f5a1abc65fe904d018981a2add27eccfd5875
Timo <timjlah@gmail.com>
2014-01-26 20:03:23 +0200

    Week 2 exercise 4.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,10 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0;
+    ratings.each { |r| sum += r.score }
+    return sum.to_f / ratings.count
+  end
 end
