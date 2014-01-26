bb544893485d7b42a001edd122c97aff24cbbd0e
Timo <timjlah@gmail.com>
2014-01-26 21:42:13 +0200

    Week 2 exercise 7.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,4 +2,15 @@ class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
   end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    rating_params = params.require(:rating).permit(:score, :beer_id)
+    beer = Beer.find(rating_params[:beer_id]) # throws if beer id not found
+    beer.ratings.create rating_params
+    redirect_to ratings_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Ratebeer::Application.routes.draw do
 
   get 'ratings', to: 'ratings#index'
 
+  get 'ratings/new', to:'ratings#new'
+
+  post 'ratings', to: 'ratings#create'
+
   get 'kaikki_bisset', to: 'beers#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
