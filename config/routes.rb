22fb45e0e92ec36f4a333a11a7021fdb807cd0af
Timo <timjlah@gmail.com>
2014-01-26 22:40:22 +0200

    Week 2 exercise 11.

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,7 @@ Ratebeer::Application.routes.draw do
 
   resources :breweries
 
-  get 'ratings', to: 'ratings#index'
-
-  get 'ratings/new', to:'ratings#new'
-
-  post 'ratings', to: 'ratings#create'
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
   get 'kaikki_bisset', to: 'beers#index'
 
