8f129be677807b01b966f2878c967bf130b73559
Timo <timjlah@gmail.com>
2014-02-02 22:36:12 +0200

    Week 3 exercise 14.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,15 +40,13 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    if @user == current_user
-      respond_to do |format|
-        if @user.update(user_params)
-          format.html { redirect_to @user, notice: 'User was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render action: 'edit' }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if user_params[:username].nil? and @user == current_user and @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
