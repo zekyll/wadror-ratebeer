8fc2872c24d156e347ff1a36c9e24bf3b18c78d4
Timo <timjlah@gmail.com>
2014-02-09 18:35:51 +0200

    Week 4 exercise 2.

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Beer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "with proper style and name is saved" do
+    beer = Beer.create style: "Style1", name: "Beer1"
+    beer.valid?.should == true
+    Beer.count.should == 1
+  end
+
+  it "without name is not saved" do
+    beer = Beer.create style: "Style1"
+    beer.valid?.should == false
+    Beer.count.should == 0
+  end
+
+  it "without style is not saved" do
+    beer = Beer.create style: "Style1"
+    beer.valid?.should == false
+    Beer.count.should == 0
+  end
+
 end
