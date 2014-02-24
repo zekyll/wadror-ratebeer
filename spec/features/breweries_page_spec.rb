716524a9322082f826d93b175aa0094681c16952
Timo <timjlah@gmail.com>
2014-02-24 23:23:51 +0200

    Test fixes.

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe "Breweries page" do
   it "should not have any before been created" do
     visit breweries_path
-    expect(page).to have_content 'Listing breweries'
-    expect(page).to have_content 'Number of breweries: 0'
+    expect(page).to have_content 'Breweries'
+    expect(page).to have_content 'Number of active breweries: 0'
+    expect(page).to have_content 'Number of retired breweries: 0'
 
   end
 
@@ -20,7 +21,7 @@ describe "Breweries page" do
     end
 
     it "lists the breweries and their total number" do
-      expect(page).to have_content "Number of breweries: #{@breweries.count}"
+      expect(page).to have_content "Number of retired breweries: #{@breweries.count}"
       @breweries.each do |brewery_name|
         expect(page).to have_content brewery_name
       end
